499a844066d96552827e1f4563b18a6c8c24bf3f
shubh-37 <shubh.arya37@gmail.com>
2023-02-10 11:24:23 +0530

    middleware setup, connected db and tested get route

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,22 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
+const connectDB = require("./db/connect");
 
-app.listen(3000, console.log("server listening on port:3000"));
+//middlewares
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("shubh is doing good");
+})
+
+try {
+    function start(){
+        connectDB(process.env.MONGO_URI);
+        app.listen(3000, console.log("server listening on port:3000"));
+    }    
+} catch (error) {
+    console.log(error);
+}
+
+start();
