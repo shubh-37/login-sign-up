e904daed62bafb21ccad604d476f2663517df662
shubh-37 <shubh.arya37@gmail.com>
2023-02-13 14:58:49 +0530

    imported auth controllers and changed way of DB connection

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,19 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 const connectDB = require("./db/connect");
+const authController = require("./controllers/auth.js");
+const auth = require("./controllers/auth.js");
 
 //middlewares
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("shubh is doing good");
-})
-
 try {
-    function start(){
-        connectDB(process.env.MONGO_URI);
-        app.listen(3000, console.log("server listening on port:3000"));
-    }    
+  async function start() {
+    const { Models } = await connectDB();
+    authController(app, Models);
+    app.listen(3000, console.log("server listening on port:3000"));
+  }
+  start();
 } catch (error) {
-    console.log(error);
+  console.error("Sorry! Cannot start the server!", error);
 }
-
-start();
