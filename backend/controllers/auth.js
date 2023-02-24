93f5fa6ff76ed4d2e43d90ed75fe6edf82922737
shubh-37 <shubh.arya37@gmail.com>
2023-02-24 13:01:47 +0530

    refact: login and sign-up controller with user-defined exception

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,13 +1,56 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+function userDefinedException(message, statusCode){
+  this.message = message;
+  this.statusCode = statusCode;
+}
 function auth(app, Models) {
   const { User } = Models;
   app.post("/register", async function register(req, res) {
     const { emailId, password } = req.body;
-    const user = await User.findOne({ emailId });
-    res.send("register a person");
+    debugger;
+    try {
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(password, salt);
+      const user = await User.create({
+        emailId: emailId,
+        password: hashedPassword,
+      });
+      console.log(user);
+      const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+      });
+      res.status(201).json({ token: token });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
   });
 
   app.post("/login", async function login(req, res) {
-    res.send("login a user");
+    const { emailId, password } = req.body;
+    try{
+      debugger;
+    if (!emailId || !password) {
+      throw new userDefinedException("Please enter both Email and Password", 401);
+    }
+    const user = await User.findOne({ emailId });
+    if (!user) {
+      throw new userDefinedException("No user found!", 404);
+    }
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      throw new userDefinedException("Wrong password!", 401);
+    }
+
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "30d",
+    });
+
+    res.status(200).json({ token });
+  }catch(error){
+      res.status(error.statusCode).json({ message: error.message });
+  }
   });
 }
 
