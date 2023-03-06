0faf5e75e7c4efffe9c7cd3ef7c473e3c165852c
shubh-37 <shubh.arya37@gmail.com>
2023-03-06 21:09:39 +0530

    added cors

--- backend/app.js
+++ backend/app.js
@@ -4,9 +4,15 @@ require("dotenv").config();
 const connectDB = require("./db/connect");
 const authController = require("./controllers/auth.js");
 const auth = require("./controllers/auth.js");
+const cors = require("cors");
 
 //middlewares
 app.use(express.json());
+app.use(cors())
+
+app.get('/', (req,res)=>{
+  res.send("shubh");
+})
 
 try {
   async function start() {
