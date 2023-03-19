2271fdb0fdedcb90952d364cee483ee84d5e7120
shubh-37 <shubh.arya37@gmail.com>
2023-03-19 17:53:17 +0530

    added the email and password checker code

---
+++ frontend/index.js
@@ -0,0 +1,34 @@
+// const { default: axios } = require("axios");
+
+const email = document.querySelector('.emailSignUp');
+const password = document.querySelector('.password-sign-up');
+const submitBtn = document.querySelector(".submit");
+const outputDiv = document.querySelector(".output");
+
+const passwordChecker = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20}$/;
+const emailChecker = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+function onSignUpHandler(){
+    const emailVal = email.value;
+    const passwordVal = password.value;
+    const data = {emailID: emailVal, password: passwordVal}
+    console.log(data)
+    if(emailVal&&passwordVal){
+        if(emailVal.match(emailChecker)&&passwordVal.match(passwordChecker)){
+            outputDiv.innerText = "Youre logged in"
+            axios.post("http://localhost:3000", data)
+              .then(function (response) {
+                console.log(response);
+              })
+              .catch(function (error) {
+                console.log(error);
+              });
+            // window.location.replace("http://127.0.0.1:5500/frontend/login.html")
+        }else{
+            outputDiv.innerText = "Please enter correct email id and password!"
+        }
+    }else{
+        outputDiv.innerText = "Please enter both email and password to proceed further."
+    }   
+}
+submitBtn.addEventListener('click',onSignUpHandler)
