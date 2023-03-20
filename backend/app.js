3c98a6557d60dc12537b5600ee405f15fcbae02a
shubh-37 <shubh.arya37@gmail.com>
2023-03-20 19:45:59 +0530

    changed the url for testing a route

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors())
 
-app.get('/frontend/login.html', (req,res)=>{
+app.get('/login.html', (req,res)=>{
   res.send("this is great");
 })
 
